78dd8ef23392c3d5be960835e75c35527e17d8c9
José Luis Vilches <jose.luis.comvilches@gmail.com>
2023-06-07 02:47:57 -0300

    Cargamos información al About desde firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,15 +9,30 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada: boolean = false;
+  equipo: any[] = [];
 
-  constructor( private http: HttpClient ) {
+  constructor(private http: HttpClient) {
 
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
-        .subscribe((resp: InfoPagina) => {
-          this.cargada = true;
-          this.info = resp;
-        })
-   }
+      .subscribe((resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+      });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-portfolio-e3e2a-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe( (resp: any) => {
+        this.equipo = resp;
+        console.log(resp);
+      });
+  }
 
 }
 
